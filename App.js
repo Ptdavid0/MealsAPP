97bf8489eb77e5ef2b4771c00f89303ac74e1f4b
Ptdavid0 <pedro_antonio.pereira@syngenta.com>
2022-06-08 11:51:19 -0300

    First screen navigation

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, SafeAreaView } from "react-native";
 import CategoryScreen from "./screen/CategoryScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <CategoryScreen />
-      <StatusBar style="light" />
-    </SafeAreaView>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Meals Categories" component={CategoryScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      <StatusBar style="dark" />
+    </>
   );
 }
 
