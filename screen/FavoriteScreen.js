d202860b04e8a222dcfea8d4d5c4a7d5e0a97c9a
Ptdavid0 <pedro_antonio.pereira@syngenta.com>
2022-06-09 11:26:21 -0300

    Adding no favorites meals message

--- screen/FavoriteScreen.js
+++ screen/FavoriteScreen.js
@@ -1,18 +1,36 @@
 import React, { useContext } from "react";
-import { View } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import MealsList from "../components/MealsList";
 import { FavoritesContext } from "../store/context/favoritesContext";
-import { MEALS } from "../data/mockData";
 
 const FavoriteScreen = () => {
   const favoritesMealsContext = useContext(FavoritesContext);
   const favoriteMeals = favoritesMealsContext.favoritesMeals;
 
   return (
-    <View>
-      <MealsList data={favoriteMeals} />
-    </View>
+    <>
+      {favoriteMeals.length ? (
+        <MealsList data={favoriteMeals} />
+      ) : (
+        <View style={styles.messageContainer}>
+          <Text style={styles.message}> You have no favorite meals</Text>
+        </View>
+      )}
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  messageContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  message: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#24180f",
+  },
+});
 
 export default FavoriteScreen;
